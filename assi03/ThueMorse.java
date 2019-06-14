fcce871c665892cdbb7c909c1920180fd12f7d82
XzJ <394386241@qq.com>
2019-06-14 18:14:46 +0800

    update files

---
+++ assi03/ThueMorse.java
@@ -0,0 +1,12 @@
+public class ThueMorse {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int length = n * n - 1;
+
+
+        int thueMorse = new int[length];
+
+
+
+    }
+}
