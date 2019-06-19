307ac52823094c4795321b6620ab7c0d42062e4b
XzJ <394386241@qq.com>
2019-06-19 14:19:33 +0800

    update files

--- assi03/Birthday.java
+++ assi03/Birthday.java
@@ -1,31 +1,42 @@
 public class Birthday {
-    private static int oneTrail(int n, int i) {
-        int[] birthday = new int[n];
-
-        int j = 0;
-        int index = (int) Math.random() * n;
-        while (birthday[index] == 0 && j < i) {
-            birthday[index] = 1;
-            index = (int) Math.random() * n;
-            j++;
+    public static int trial(int n) {
+        int choosedDay = (int) (Math.random() * n);
+        for (int i = 0; i < n; i++) {
+            int nextDay = (int) (Math.random() * n);
+            if (nextDay == choosedDay) return i;
         }
-        if (j == i) return 0;
-        else return 1;
+        return 0;
+    }
+
+    public static void count(int n, int trails) {
+        int[] count = new int[n];
+        double[] fraction = new double[n];
+        for (int i = 0; i < trails; i++) {
+            count[trial(n)]++;
+        }
+        for (int i = 0; i < fraction.length; i++) {
+            double total = 0.0;
+            for (int j = 0; j < i; j++) {
+                total += count[i];
+            }
+            fraction[i] = total / trails;
+        }
+        showData(count, fraction);
+    }
+
+    public static void showData(int[] count, double[] fraction) {
+        for (int i = 1; i <= count.length; i++) {
+            System.out.printf("%5d%10d%10f\n", i - 1, count[i - 1], fraction[i - 1]);
+        }
     }
 
     public static void main(String[] args) {
 //        int n = Integer.parseInt(args[0]);
 //        int trials = Integer.parseInt(args[1]);
         int n = 365;
-        int trials = 1000000;
+        int trials = 1000;
 
-        int count = 0;
-        for (int k = 0; k < trials; k++) {
-            count += oneTrail(n, 2);
-        }
-
-        System.out.println(count);
-
+        count(n, trials);
 
     }
 }
