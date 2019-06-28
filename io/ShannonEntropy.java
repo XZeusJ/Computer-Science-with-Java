d13275c64ab9befc7d856629f9173004687d4ab8
XzJ <394386241@qq.com>
2019-06-28 22:48:37 +0800

    update files

--- io/ShannonEntropy.java
+++ io/ShannonEntropy.java
@@ -1,26 +1,25 @@
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
-
 public class ShannonEntropy {
     public static void main(String[] args) {
-        int m = Integer.parseInt(args[0]);
-        double[] a = new double[m+1];
+        int m;
+        if (args.length != 0) m = Integer.parseInt(args[0]);
+        else m = 100;
 
         int len = 0;
+        int[] freq = new int[m + 1];
         while (!StdIn.isEmpty()) {
             int x = StdIn.readInt();
-            a[x]++;
+            freq[x]++;
             len++;
         }
 
         double shannon = 0.0;
         for (int j = 1; j <= m; j++) {
-            double pi = 1.0 * a[j]/len;
+            double pi = 1.0 * freq[j] / len;
 //            StdOut.println("a[j] "+a[j]+" pi "+pi);
-            if (pi != 0) shannon -= pi * (Math.log(pi)/Math.log(2));
+            if (pi != 0) shannon -= pi * (Math.log(pi) / Math.log(2));
         }
 
 
-        StdOut.printf("%5.4f\n", shannon);
+        StdOut.printf("%.4f\n",shannon);
     }
 }
--- io/WorldMap.java
+++ io/WorldMap.java
@@ -1,23 +1,31 @@
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
-
 public class WorldMap {
     public static void main(String[] args) {
         int width = StdIn.readInt();
         int height = StdIn.readInt();
+        StdDraw.setCanvasSize(width, height);
+        StdDraw.setXscale(0, width);
+        StdDraw.setYscale(0, height);
+        StdDraw.enableDoubleBuffering();
 
-        StdOut.println("width is:" + width);
-        StdOut.println("height is:" + height);
-
-        int i = 0;
-        while (StdIn.hasNextLine() && i < 10) {
-
-
-            String line = StdIn.readLine();
-            StdOut.println(line);
-
-            i++;
+        int posNum;
+        double[] aX;
+        double[] aY;
+        while (!StdIn.isEmpty()) {
+            String line = StdIn.readString();
+            if (line.matches(".*[a-zA-Z\\_]+.*")) {
+//                StdOut.println(line);
+                posNum = StdIn.readInt();
+                aX = new double[posNum];
+                aY = new double[posNum];
+                for (int i = 0; i < posNum * 2; i = i + 2) {
+                    aX[i/2] = StdIn.readDouble();
+                    aY[i/2] = StdIn.readDouble();
+//                    StdOut.println(aX[i/2]+"  "+aY[i/2]);
+                }
+                StdDraw.polygon(aX, aY);
+            }
         }
+        StdDraw.show();
 
     }
 }
