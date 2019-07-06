1a2351244bdc27acc765fcf2bf1b0dfae8de6b58
XzJ <394386241@qq.com>
2019-07-06 19:15:25 +0800

    update files

--- performance/Inversions.java
+++ performance/Inversions.java
@@ -4,6 +4,8 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
+import java.util.NoSuchElementException;
+
 public class Inversions {
     // Return the number of inversions in the permutation a[].
     public static long count(int[] a) {
@@ -19,29 +21,41 @@ public class Inversions {
 
     // Return a permutation of length n with exactly k inversions.
     public static int[] generate(int n, long k) {
+        if (!(n >= 0)) throw new NoSuchElementException("n is illegal");
+        if (!((0 <= k) && (k <= (1.0 / 2) * n * (n - 1))))
+            throw new NoSuchElementException("k is illegal");
+
         int[] a = new int[n];
         int eachN = n;
         int i = 0;
-        while (k > n) {
-            eachN -= 1;
-            a[i] = eachN;
-            k -= eachN;
+        int normal = 0;
+        while (i < n) {
+            if (k >= n-i-1) {
+                eachN -= 1;
+                a[i] = eachN;
+                k -= eachN;
+            } else {
+                a[i] = normal++;
+            }
             i++;
         }
-
         return a;
     }
 
     // Takes an integer n and a long k as command-line arguments,
     // and prints a permutation of length n with exactly k inversions.
     public static void main(String[] args) {
-        // int n = Integer.parseInt(args[0]);
-        // int k = Integer.parseInt(args[1]);
+        int n = Integer.parseInt(args[0]);
+        int k = Integer.parseInt(args[1]);
 
-        // generate(n, k);
+        int[] a = generate(n, k);
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
 
         // test count
-        int[] a = {9,8,0,1,2,3,7,4,5,6};
-        StdOut.println(count(a));
+        // int[] a = { 9, 8, 0, 1, 2, 3, 7, 4, 5, 6 };
+        // StdOut.println(count(a));
     }
 }
--- performance/Ramanujan.java
+++ performance/Ramanujan.java
@@ -4,10 +4,30 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
-package PACKAGE_NAME;
-
 public class Ramanujan {
-    public static void main(String[] args) {
+    // Is n a Ramanujan number?
+    public static boolean isRamanujan(long n) {
+        int count = 0;
+        double max =  Math.ceil(Math.pow(n, 1.0/3));
+        StdOut.println("max is:" + max);
 
+
+        return count == 2;
+    }
+
+    // Takes a long integer command-line arguments n and prints true if
+    // n is a Ramanujan number, and false otherwise.
+    public static void main(String[] args) {
+        //  n = Integer.parseInt(args[0]);
+        long n = 1729;
+        StdOut.println(isRamanujan(n));
+        n = 3458;
+        StdOut.println(isRamanujan(n));
+        n = 4104;
+        StdOut.println(isRamanujan(n));
+        n = 216125;
+        StdOut.println(isRamanujan(n));
+        // n = 9223278330318728221L;
+        // StdOut.println(isRamanujan(n));
     }
 }
